0d176f0c7c0ee43c59b624d686ccd273bcac0a51
Matt <matthew.salt@gmail.com>
2016-04-23 14:05:23 +0100

    Add the flash
    Add flash episode

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
 var requester = require('request')
 var cheerio = require('cheerio')
 
-var url = 'http://flash-arrow-order.herokuapp.com/hide/flash+legends/'
 
-requester(url, function (error, response, body) {
+var arrowUrl = 'http://arrow.wikia.com/wiki/List_of_Arrow_episodes'
+var flashUrl = 'http://arrow.wikia.com/wiki/List_of_The_Flash_episodes'
+
+var episodes = []
+
+
+requester(arrowUrl, function (error, response, body) {
   if (!error) {
-  	var $ = cheerio.load(body)
-      	var rows = $('.episode')
-      	for( var i = 0 ; i < rows.length ; i++){
-      		var data =rows.eq(i).children('td') //rows.eq(i) == $(rows[i])
-      		var message = data.eq(0).html().trim() + ":"+ $(data[3]).html().trim()
-      	    // var message = data[0]['children'][0]['data'].trim() + ":" + data[3]['children'][0]['data'].trim()
-      		console.log(message)
-      	}
+    var $ = cheerio.load(body)
+        var tables = $('.wikitable')
+        for (var i = 0 ; i < tables.length ; i++){
+             var table = tables[i]
+             var rows = $(table).children('tr')
+             $(rows).each(function(item){
+                var data = $(this).children('td')
+                if (data.eq(2).text().trim().length > 0){
+                  var episode = {
+                    series:'Arrow',
+                    name:data.eq(2).text().trim().slice(0, -1).slice(1),
+                    number:'S'+ (i+1) + 'E' + data.eq(1).text().trim(),
+                    airdate: data.eq(3).text().trim()
+                  }
+                  episodes.push(episode)
+                }
+             })
+        }
+      }})
 
-        	
+requester(flashUrl, function (error, response, body) {
+  if (!error) {
+    var $ = cheerio.load(body)
+        var tables = $('.wikitable')
+        for (var i = 0 ; i < tables.length ; i++){
+             var table = tables[i]
+             var rows = $(table).children('tr')
+             $(rows).each(function(item){
+                var data = $(this).children('td')
+                if (data.eq(2).text().trim().length > 0){
+                  var episode = {
+                    series:'Flash',
+                    name:data.eq(2).text().trim().slice(0, -1).slice(1),
+                    number:'S'+ (i+1) + 'E' + data.eq(1).text().trim(),
+                    airdate: data.eq(3).text().trim()
+                  }
+                  episodes.push(episode)
+                }
+             })
+        }
+  console.log(episodes)
+  episodes = episodes.sort(function(a, b) {
+    console.log((new Date(a.airdate) > new Date(b.airdate)))
+    return ( new Date(a.airdate) > new Date(b.airdate) ? -1 : 1);
+  });
+  console.log(episodes[0])
+  console.log(episodes[1])
+
+  var c = new Date('October 10, 2012')
+  var d = new Date('October 7, 2015')
+
+  console.log(c > d)
+
   } else {
     console.log("We’ve encountered an error: " + error);
   }
-
 });
+
+
+
--- source.js
+++ source.js
@@ -1,46 +1,104 @@
 var requester = require('request')
 var cheerio = require('cheerio')
 var http = require('http')
+var async = require('async')
 var port = 3100
 
 var pug=require('pug')
 var fs = require('fs')
 
-var episodes = new Object()
+var episodes = []
+
+var server = http.createServer();
+
+var requestHandler = function(request, response){
+    if(request.url === '/view' ){
 
 
-var server = http.createServer(function(request, response){
-  response.write(pug.renderFile('./view.pug',{data:episodes}))
-  response.end()
-})
+      response.write(pug.renderFile('./view.pug',{data:episodes}))
+      response.end()
+  }else{
+      response.statuscode = 404
+      response.end()
+  }
+}
+
+server.on('request', requestHandler)
+
 
 server.listen(port, function(){
    console.log('Server listening on port ' + port)
 })
 
-var url = 'http://arrow.wikia.com/wiki/List_of_Arrow_episodes'
+var arrowUrl = 'http://arrow.wikia.com/wiki/List_of_Arrow_episodes'
+var flashUrl = 'http://arrow.wikia.com/wiki/List_of_The_Flash_episodes'
 
-
-requester(url, function (error, response, body) {
+var task1 = function(callback){
+  requester(arrowUrl, function (error, response, body) {
   if (!error) {
   	var $ = cheerio.load(body)
-      	var rows = $('tr')
+      	var tables = $('.wikitable')
+        for (var i = 0 ; i < tables.length ; i++){
+             var table = tables[i]
+             var rows = $(table).children('tr')
       	$(rows).each(function(item){
             var data = $(this).children('td')
 
             if (data.eq(2).text().trim().length > 0){
-            var episode = {
-              name:data.eq(2).text().trim().slice(0, -1).slice(1),
-              number:data.eq(0).text().trim(),
-              airdate: data.eq(3).text().trim()
-            }
-            episodes[data.eq(0).text().trim()] = episode
-          }
-          })
-        console.log(episodes)
+                 var episode = {
+                    series:'Arrow',
+                    name:data.eq(2).text().trim().slice(0, -1).slice(1),
+                    number:'S'+ (i+1) + 'E' + data.eq(1).text().trim(),
+                    airdate: data.eq(3).text().trim()
+                 }
+                 // episodes[data.eq(0).text().trim()] = episode
+                                   episodes.push(episode)
 
+            }
+          })
+      }
   } else {
     console.log("We’ve encountered an error: " + error);
   }
+  callback(null)
 });
+}
 
+var task2 = function(callback) {
+  requester(flashUrl, function (error, response, body) {
+  if (!error) {
+    var $ = cheerio.load(body)
+        var tables = $('.wikitable')
+        for (var i = 0 ; i < tables.length ; i++){
+             var table = tables[i]
+             var rows = $(table).children('tr')
+             $(rows).each(function(item){
+                var data = $(this).children('td')
+                if (data.eq(2).text().trim().length > 0){
+                  var episode = {
+                    series:'Flash',
+                    name:data.eq(2).text().trim().slice(0, -1).slice(1),
+                    number:'S'+ (i+1) + 'E' + data.eq(1).text().trim(),
+                    airdate: data.eq(3).text().trim()
+                  }
+                  // episodes[data.eq(0).text().trim()] = episode
+                  episodes.push(episode)
+                }
+             })
+        }
+  } else {
+    console.log("We’ve encountered an error: " + error);
+  }
+  callback(null)
+
+});
+}
+
+var asyncTasks = [task1, task2];
+
+async.parallel(asyncTasks, function(err, result){
+      episodes.sort(function(a, b) {
+         return (new Date(a.airdate) < new Date(b.airdate)) ? -1 : 1;
+      });
+      console.log('--SORTING COMPLETE--')
+});
